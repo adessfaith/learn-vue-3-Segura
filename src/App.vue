a6a5d4da91339d9b0f4ff46f7b4448fc583916d7
adessfaith <adessasegura@gmail.com>
2025-02-23 23:57:12 -0600

    OneWayTwoWay

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,26 @@
 <template>
+  <h1>v-model vs v-bind</h1>
+  <h2>message: {{ message }}</h2>
   <div>
-    Hello, Vue!
+    <p>v-model = v-bind + @input</p>
+    <input
+      type="text"
+      v-bind:value="message"
+      @input="message = $event.target.value"
+    />
+  </div>
+  <div>
+    <p>v-model</p>
+    <input type="text" v-model="message" />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+let message = ref('Hello!')
 </script>
 
-<style lang="scss" scoped>
-
-</style>
-
-
-<!--<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
-!-->
