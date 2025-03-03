b560b51dbbbff229caaf79d9e9fac6d97113a730
adessfaith <adessasegura@gmail.com>
2025-03-02 18:36:04 -0600

    watchers

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,103 @@
 <template>
-  <h1>
-    {{ message }}
-  </h1>
-  <button @click="sortUsersByAge">Sort users by age</button>
-  <!-- <button @click ="hideInactive !=hideInactive"> {{ hideInactive ? 'Show all': 'Hide inactive' }}</button> -->
-  <button @click="hideInactive = !hideInactive">{{ toggleButtonName }}</button>
+  <h1>{{ message }}</h1>
+  <div class="card">
+    <h1>Sceneriod 1: Watch a "ref(primitive value)"</h1>
+    <h2>Number: {{ number }}</h2>
+    <button @click="number++">Increment number by 1</button>
+  </div>
 
-  <!-- <h2>Numbers of active users: {{ users.filter((user)=>user.isActive).length }}</h2> dont couple user logic with UI -->
-  <h2>Numbers of active users: (computed property){{ numberOfActiveUsers }}</h2>
-  <h2>Numbers of active users: (computed property){{ numberOfActiveUsers }}</h2>
-  <h2>Numbers of active users: (computed property){{ numberOfActiveUsers }}</h2>
-
-  <h2>
-    Numbers of active users:(method call) {{ computedNumberOfActiveUsers }}
-  </h2>
-  <h2>
-    Numbers of active users:(method call) {{ computedNumberOfActiveUsers }}
-  </h2>
-  <h2>
-    Numbers of active users:(method call) {{ computedNumberOfActiveUsers }}
-  </h2>
-  <!--A computer property will only re-evaluate, not cache the result,  or computed when some of its reactive dependencies have changed-->
-
-  <table>
-    <tr>
-      <th>Index</th>
-      <th>Id</th>
-      <th>Name</th>
-      <th>Age</th>
-      <th>Operation</th>
-    </tr>
-    <tr v-for="(user, index) in filteredUsers" :key="user.id">
-      <td>{{ index + 1 }}</td>
-      <td>{{ user.id }}</td>
-      <td :class="{ inactive: !user.isActive }">{{ user.name }}</td>
-      <td>{{ user.age }}</td>
-      <td>
-        <button @click="user.isActive = !user.isActive">
-          {{ user.isActive ? "Deactivate" : "Restore" }}
-        </button>
-      </td>
-    </tr>
-  </table>
+  <div class="card">
+    <h1>Scenerio 2: watch a property in a "ref(object)"</h1>
+    <h2>Name: {{ wizard1.name }}</h2>
+    <h2>Wand" {{ wizard1.wand }}</h2>
+    <button @click="wizard1.name = wizard1.name.toUpperCase()">
+      Change name to UPPER CASE
+    </button>
+    <button @click="changeWizardWand">Change wand</button>
+    <button @click="wizard1.wand.core = 'Unicorn hair'">
+      Change wand core
+    </button>
+  </div>
+  <div class="card">
+    <h1> Scenerio 3: watch a "ref(object)"</h1>
+    <h1>Name: {{ wizard2.name }}</h1>
+    <h2>Wand : {{ wizard2.wand }}</h2>
+    <button @click="wizard2.name = wizard2.name.toUpperCase()"> Change name to UPPERCASE</button>
+    <button @click=" wizard2.wand.core = 'Pheonix feather'"> Change wand core</button>
+    
+  <button @click ="changeWizard"> Change wizard </button>
+</div>
 </template>
 <script setup>
-import { computed, ref } from "vue";
+import { watch, ref } from "vue";
+let message = ref("Hello, Watchers!");
+//scenerio 1: watch a ref(primitive value), this is a
+let number = ref(1);
+let stopWatch = watch(
+  number,
+  (newValue, oldValue) => {
+    console.log(
+      "Watch a ref(primitive value): number changes",
+      newValue,
+      oldValue
+    );
+    if (newValue > 5) {
+      stopWatch();
+    }
+  },
+  { immediate: true }
+); //for immediate watching
 
-let message = ref("Hello, Computed Properties!");
-const users = ref([
-  { id: 1001, name: "John smith", age: 26, isActive: false },
-  { id: 1002, name: "Tom Doe", age: 16, isActive: false },
-  { id: 1003, name: "Franklin Wong", age: 18, isActive: true },
-]);
-function sortUsersByAge() {
-  users.value.sort((a, b) => a.age - b.age);
+//scenerio 2: Watch a property in a ref(object)
+let wizard1 = ref({
+  id: 1001,
+  name: "Harry Potter",
+  house: "Gryffindor",
+  age: 17,
+  wand: { core: "Phoenix feather", wood: "Holly" },
+});
+function changeWizardWand() {
+  wizard1.value.wand = { core: "Dragon heartstring", wood: "Vine" };
 }
-let numberOfActiveUsers = computed(() => {
-  console.log("computed property");
-  return users.value.filter((user) => user.isActive).length;
-});
-console.log(numberOfActiveUsers.value); //automatically tracks its dependencies
+ let wizard2 = ref({id: 1003, name:' Ron Wealy', house: 'Gryffindor', age: 17, wand: { core: 'Unicorn hair',wood: 'Willow'}});
 
-let hideInactive = ref(false);
+//  watch(wizard1.value.name, (newValue, oldValue)=>{})  not valid, watch object can only be a ref, a reactive object, a getter function, or an array of these types of data
+watch(
+  () => wizard1.value.name,
+  (newValue, oldValue) => {
+    console.log(
+      "Watch a property in a ref(object): wizard1 name changes",
+      newValue,
+      oldValue
+    );
+  }
+);
 
-let filteredUsers = computed(() =>
-  hideInactive.value ? users.value.filter((user) => user.isActive) : users.value
+watch(
+  () => wizard1.value.wand,
+  (newValue, oldValue) => {
+    // Address or reference
+    console.log(
+      "watch a proprty in a ref(object): wizard1 wand changes",
+      newValue,
+      oldValue
+    );
+  },
+  { deep: true }
 );
-let toggleButtonName = computed(() =>
-  hideInactive.value ? "Show all" : "Hide inactive"
-);
-let computedNumberOfActiveUsers = computed(() => {
-  console.log("method call");
-  return users.value.filter((user) => user.isActive).length;
-});
+
+function changeWizard(){
+  wizard2.value = { id: 1002, name: 'Hermione Granger', house: 'Gryffindor', age: 17, wand: {core: 'Dragon heartstring', wood: 'Vine'}}}
+
+  watch (wizard2), (newValue, oldValue)=>{console.log('watch a ref(object): wizard2 changes', newValue, oldValue)},{deep:true};
+
+  watch([number, ()=>wizard1.value.name, wizard2],(newValue, oldValue)=>{console.log("Watch an array of ref(primitive value, watch a property of a ref object, watch ), a property of 'ref' object, 'ref object'")},{deep: true})
 </script>
 <style scoped>
-.inactive {
-  color: red;
-  text-decoration: line-through;
+.card {
+  background-color: purple;
+  color: white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
 }
 </style>
