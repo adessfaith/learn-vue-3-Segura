d63b136f7ecd7eef7d4abb9ea4e63a567595ccab
adessfaith <adessasegura@gmail.com>
2025-03-08 14:32:33 -0600

    introduction to git components

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,20 @@
 <template>
-  <h1>{{ message }}</h1>
-  <div class="card">
-    <h2 ref="title">This the App component.</h2>
-    <h2>Number: {{ number }}</h2>
-    <button @click="number++">Increment number by one</button>
-    <button @click="isShow =!isShow"> Toggle Component1</button>
+  <ButtonCounter></ButtonCounter>
+  <!--Pascal Case-->
+  <ButtonCounter></ButtonCounter>
 
-    <Component1 v-if="isShow"></Component1>
+  <button-counter></button-counter>
+  <!--Button do not recognize non-standard custum tags-->
+  <hr />
+  <div style="background-color: orange; padding: 5px">
+    This is the root component.
+    <ComponentA></ComponentA>
+    <ComponentA></ComponentA>
   </div>
-
 </template>
 <script setup>
-import {
-  ref,
-  onBeforeMount,
-  onMounted,
-  onBeforeUpdate,
-  onUpdated,
-  onBeforeUnmount,
-  onUnmounted,
-  watch 
-} from "vue";
-
-import Component1 from './Component1.vue';
-
-let isShow = ref(true);
-
-let message = ref("Hello, Lifecyle Hooks!");
-
-let number = ref(1);
-let title = ref();
-
-console.log("App component is setup"); // Order 1
-
-onBeforeMount(() => {
-  console.log("App component is before mount."); // Order 2
-  console.log(number.value); //Order 3
-  console.log(title.value); //Order 4 / undefined
-});
-//callback to be called after a hok has been mounted
-//good for fetching data, and is the most commonly used lifecycle hook function
-onMounted(() => {
-  console.log("App component is mounted");
-  console.log(title.value);
-});
-onBeforeUpdate(() => {
-  console.log("App component is before update.");
-});
-onUpdated(() => {
-  console.log("App component is updated.");
-});
-onBeforeUnmount(() => {
-  console.log("App component is before unmount");
-});
-onUnmounted(() => {
-  console.log("App component is unmounted.");
-});
-
-watch(number, ()=>{console.log('number changes')});
-
-
-
-
+import ButtonCounter from "./ButtonCounter.vue";
+import ComponentA from "@/COmponentA.vue";
 </script>
-<style scoped>
-.card {
-  background-color: purple;
-  color: white;
-  padding: 20px 10px;
-  margin-bottom: 10px;
-}
-</style>
+
+<style></style>
---
+++ src/ComponentB.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="background-color: lightblue; padding-left: 10px">
+    This is the grandchild component.
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
+
+<!--C:\Users\afaithsegura\Desktop\learn-vue-3-Segura\src\ComponentB.vue-->
+<!--src\ComponentB.vue-->
