59accc77a64491e8db9600f51967d7dd2e935ff2
adessfaith <adessasegura@gmail.com>
2025-03-09 14:05:45 -0500

    pass data through props

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,94 @@
 <template>
-  <ButtonCounter></ButtonCounter>
-  <!--Pascal Case-->
-  <ButtonCounter></ButtonCounter>
-
-  <button-counter></button-counter>
-  <!--Button do not recognize non-standard custum tags-->
-  <hr />
   <div style="background-color: orange; padding: 5px">
-    This is the root component.
-    <ComponentA></ComponentA>
-    <ComponentA></ComponentA>
+    <p>This is the App component</p>
+    <h2>{{ blogPostId }} - {{ blogPostTitle }}</h2>
+    <h4>
+      {{ blogPostContent }}
+    </h4>
+    <button @click="blogPostTitle = 'A different title'">
+      Change Blog Post Title
+    </button>
+    <!--when a parent data changes, it will flow down, but when child changes, data will not flow up-->
+    <BlogPost
+      :id="blogPostId"
+      :blog-post-title="blogPostTitle"
+      :blog-post-content="blogPostContent"
+    ></BlogPost>
+    <BlogPost
+      :id="blogPostId"
+      :blog-post-title="blogPostTitle"
+      :blog-post-content="blogPostContent"
+    ></BlogPost>
   </div>
+  <hr />
+  <BlogPost
+    id="1"
+    blogPostTitle="What is a muggle?"
+    blogPostContent="A muggle is a person who lacks any sort of magical ability ..."
+  >
+  </BlogPost>
+  <!--use kebab case-->
+  <BlogPost
+    id="1"
+    blog-post-title="What is a muggle?"
+    blog-post-content="A muggle is a person who lacks any sort of magical ability"
+  ></BlogPost>
+  <BlogPost
+    :id="blogPostId"
+    :blog-post-title="blogPostTitle"
+    :blog-post-content="blogPostContent"
+  ></BlogPost>
+
+  <BlogPost
+    v-for="posts in posts"
+    :key="posts.id"
+    :id="posts.id"
+    :blog-post-title="posts.blogPostTitle"
+    :blog-post-content="posts.blogPostContent"
+  ></BlogPost>
+  <hr />
+  <BlogPost v-for="posts in posts" :key="posts.id" v-bind="posts"></BlogPost>
 </template>
+
 <script setup>
-import ButtonCounter from "./ButtonCounter.vue";
-import ComponentA from "@/COmponentA.vue";
+import BlogPost from "./BlogPost.vue";
+
+import { ref } from "vue";
+
+let blogPostId = ref(2);
+let blogPostTitle = ref(
+  "HP and the Cursed Child production suspended until April"
+);
+let blogPostContent = ref(
+  "Adding to the earlier post, the production has been ...."
+);
+let posts = ref([
+  {
+    id: 1,
+    blogPostTitle: "What is a muggle in HP world?",
+    blogPostContent:
+      "A muggle is a person who lacks any sort of magical ability...",
+  },
+  {
+    id: 2,
+    blogPostTitle:
+      "HP and the Cursed Child Broadway production suspended until April",
+    blogPostContent:
+      "Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...",
+  },
+  {
+    id: 3,
+    blogPostTitle: "Potter DIY: Make Your Own “Harry Potter Puppet Pal”",
+    blogPostContent:
+      "We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...",
+  },
+  {
+    id: 4,
+    blogPostTitle: "Fan Project Brings Life to “Harry Potter” in Translation",
+    blogPostContent:
+      "It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....",
+  },
+]);
 </script>
 
-<style></style>
+<style scoped></style>
---
+++ src/BlogPost.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class = 'blog-post'>
+        <p>
+            {{ message }}
+        </p>
+        <!--Display the id, title, content-->
+        <h2> {{ id }} - {{ blogPostTitle }}</h2>
+        <h4> {{blogPostContent}}</h4>
+        <button @click="changeBlogPostTitle"> Change Blog Post Title</button>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+let message = ref('This is the BlogPost component.');
+const props = defineProps(['id','blogPostTitle','blogPostContent']); //returns an object with all the props, so we can access it outside the script
+console.log(props.blogPostContent);
+// defineProps({id:Number, blogPostTitle:String, blogPostContent:String});
+function changeBlogPostTitle(){
+    props.blogPostTitle = "A different title";
+}
+</script>
+
+<style scoped>
+.blog-post{
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+</style>
