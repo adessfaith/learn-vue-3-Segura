87979eac5445bf6ac980ec866970798b751f86af
adessfaith <adessasegura@gmail.com>
2025-03-09 22:53:40 -0500

    provide and inject

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,29 @@
 <template>
-  <BlogPost
-    v-for="post in posts"
-    :key="post.id"
-    v-bind="post"
-    @delete-blog-post="processDeletion"
-    :id="post.id"
-    :blog-post-title="post.blogPostTitle"
-    :blog-post-content="post.blogPostContent"
-    v-model:blogPostContent = "post.blogPostContent"
-    v-model:blogPostTitle = "post.blogPostTitle"
-  ></BlogPost>
-</template>
+  <h1>Message: {{ message }}</h1>
+  <div style="background-color: orange; padding-left: 10px" >
+    <ComponentA></ComponentA>
+  </div>
+ </template>
+ 
+ <script setup>
+ import {ref, provide, readonly} from 'vue';
 
-<script setup>
-import { ref } from "vue";
-import BlogPost from "./BlogPost.vue";
-let posts = ref([
-  {
-    id: 1,
-    blogPostTitle: "What is a muggle in HP world?",
-    blogPostContent:
-      "A muggle is a person who lacks any sort of magical ability...",
-  },
-  {
-    id: 2,
-    blogPostTitle:
-      "HP and the Cursed Child Broadway production suspended until April",
-    blogPostContent:
-      "Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...",
-  },
-  {
-    id: 3,
-    blogPostTitle: "Potter DIY: Make Your Own “Harry Potter Puppet Pal”",
-    blogPostContent:
-      "We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...",
-  },
-  {
-    id: 4,
-    blogPostTitle: "Fan Project Brings Life to “Harry Potter” in Translation",
-    blogPostContent:
-      "It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....",
-  },
-]);
+ import ComponentA from './ComponentA.vue';
+ 
+ let message = ref('Hello, Provide/inject');
 
-function processDeletion(id) {
-  let index = posts.value.findIndex((item) => item.id == id);
-  posts.value.splice(index, 1);
+ provide('msg', {message, updateMessage});
+
+function updateMessage(){
+  message.value = 'Hello, Provide/Inject! Updated!';
 }
-</script>
 
-<style scoped></style>
+let count = ref(0);
+provide('read-only-count',readonly(count));
+
+ 
+ </script>
+ 
+ <style scoped>
+ 
+ </style>
---
+++ src/ComponentB.vue
@@ -0,0 +1,21 @@
+<template>
+   
+    <div style="background-color: lightblue;">
+        <h2>This is component B.</h2>
+        {{ messageFromApp}}
+
+        <button @click="updateMessage"> Update Message </button>
+    </div>
+</template>
+
+<script setup>
+import { inject } from 'vue';
+let {message: messageFromApp, updateMessage} = inject('msg', 'default message');
+
+// it recommended to kepp any mutations to reactive state inside of the provider whenever possible
+
+</script>
+
+<style scoped>
+
+</style>
