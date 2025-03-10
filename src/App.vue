04e54424de60841c38ca48cf7ce31df0112f77c9
adessfaith <adessasegura@gmail.com>
2025-03-10 09:28:56 -0500

    scoped

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
-  <BaseLayout>Header, Main, Footer</BaseLayout>
-  <BaseLayout>
-    <template #header> <h1>title</h1></template>
-    <template #main>
-      <p>Main Content</p>
+  <StudentList :list="list">
+    <template #default="{ stud }">
+      <span :class="{ cursed: stu.name == 'Harry' }">
+        {{ stu.name }}
+      </span>
     </template>
-    <template #footer>
-      <p>Contact Info!</p>
-    </template>
-  </BaseLayout>
+  </StudentList>
+
+  <hr/>
+
+  <e1-table :data="todoList" stripe border style="width:100%">
+    <e1-table-column prop="userId" label="User ID" width="180"> 
+    </e1-table-column prop="id" label = "ID" width="180">
+    <e1-table-column prop="title" label="title" > 
+    </e1-table-column>
+    <e1-table-column prop="completed" label ="Status"> 
+      <template><e1-tag type="success" v-if="slotProps.row.completed">Completed</e1-tag>
+        <e1-tag type="danger" v-else> Incomplete</e1-tag></template>
+    
+  </e1-table-column>
+  </e1-table>
 </template>
 
 <script setup>
-import BaseLayout from "./BaseLayout.vue";
+import StudentList from "./StudentList.vue";
+import {onMounted, ref} from 'vue';
+
+const list = ref([{id:1, name:'Harry'}, {id:2, name: 'Hermione'},{id:3, name: 'Ron'}]);
+
+async function getTodoList(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await response.json();
+  console.log(data);
+  todoList.value = data;
+
+}
+
+onMounted(()=>{ getTodoList();})
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.cursed {
+  color: red;
+}
+</style>
---
+++ src/StudentList.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>
+    <li v-for="stuent in list" :key="student.id"></li>
+    <Slot :stu="student">
+        {{ student.name }}
+    </Slot>
+  </ul>
+</template>
+
+<script setup>
+defineProps(['list']);
+</script>
+
+<style scoped></style>
