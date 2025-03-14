d6fbb36176c457db36b52fea7be444e868d25988
adessfaith <adessasegura@gmail.com>
2025-03-14 18:30:09 -0500

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,50 @@
 <template>
   <StudentList :list="list">
-    <template #default="{ stud }">
-      <span :class="{ cursed: stu.name == 'Harry' }">
-        {{ stu.name }}
-      </span>
+    <template #default="{ stu }">
+      <span :class="{ cursed: stu.name == 'Harry' }"> {{ stu.name }}</span>
     </template>
   </StudentList>
 
-  <hr/>
+  <!-- <span :class="{ cursed: stu.name == 'Harry' }">
+        {{ stu.name }}</span
+      > -->
 
-  <e1-table :data="todoList" stripe border style="width:100%">
-    <e1-table-column prop="userId" label="User ID" width="180"> 
-    </e1-table-column prop="id" label = "ID" width="180">
-    <e1-table-column prop="title" label="title" > 
-    </e1-table-column>
-    <e1-table-column prop="completed" label ="Status"> 
-      <template><e1-tag type="success" v-if="slotProps.row.completed">Completed</e1-tag>
-        <e1-tag type="danger" v-else> Incomplete</e1-tag></template>
-    
-  </e1-table-column>
-  </e1-table>
+  <hr />
+  <el-table :data="toDoList" stripe border style="width: 100%">
+    <el-table-column prop="userId" label="User ID" width="180" />
+    <el-table-column prop="id" label="ID" width="180" />
+    <el-table-column prop="title" label="Title" />
+    <el-table-column prop="completed" label="Status" >
+      <template #default="slotProps">
+        <el-tag type="success" v-if="slotProps.row.completed">Completed</el-tag>
+        <el-tag type="danger" v-else>Incomplete</el-tag>
+
+      </template>
+  
+    </el-table-column>
+ 
+  </el-table>
 </template>
 
 <script setup>
+import { ref , onMounted} from "vue";
 import StudentList from "./StudentList.vue";
-import {onMounted, ref} from 'vue';
 
-const list = ref([{id:1, name:'Harry'}, {id:2, name: 'Hermione'},{id:3, name: 'Ron'}]);
-
+const list = ref([]);
+const toDoList = ref([]);
 async function getTodoList(){
+
   const response = await fetch('https://jsonplaceholder.typicode.com/todos');
   const data = await response.json();
   console.log(data);
-  todoList.value = data;
+  toDoList.value = data;
+
 
 }
-
-onMounted(()=>{ getTodoList();})
+onMounted(()=>{getTodoList()});
 </script>
 
 <style scoped>
-
 .cursed {
   color: red;
 }
--- src/StudentList.vue
+++ src/StudentList.vue
@@ -1,14 +1,20 @@
 <template>
   <ul>
-    <li v-for="stuent in list" :key="student.id"></li>
-    <Slot :stu="student">
-        {{ student.name }}
-    </Slot>
+    <li v-for="student in list" :key="student.id">
+      <slot :stu="student"> {{ student.name }}</slot>
+    </li>
   </ul>
 </template>
 
 <script setup>
-defineProps(['list']);
+import { ref } from "vue";
+//import StudentList from "./StudentList.vue";
+defineProps(["list"]);
+const list = ref([
+  { id: 1, name: "Harry" },
+  { id: 2, name: "Hermione" },
+  { id: 3, name: "Ron" },
+]);
 </script>
 
 <style scoped></style>
