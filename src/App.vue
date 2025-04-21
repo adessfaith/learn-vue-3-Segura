72e007ead5d47f6c88374138cbccb5e1d4de2fac
adessfaith <adessasegura@gmail.com>
2025-04-21 13:39:43 -0500

    Nested route and dynamic route matching

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class ="app">
+  <div class="app">
     <Navigation></Navigation>
     <router-view></router-view>
-
   </div>
 </template>
 
@@ -12,11 +11,9 @@ import Navigation from '@/components/Navigation.vue';
 </script>
 
 <style lang="scss" scoped>
-.app{
-  gap: lrem;
+.app {
   display: flex;
   flex-direction: column;
-
+  gap: 1rem;
 }
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import {createRouter,createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import BlogPosts from '@/views/BlogPosts.vue'
 import About from '@/views/About.vue'
+import BlogPost from '@/views/BlogPost.vue'
+import BlogPostsGreeting from '@/views/BlogPostsGreeting.vue'
+import NotFound from '@/views/NotFound.vue'
 const router = createRouter({
 
     //provide the history implementation to use
@@ -10,8 +13,22 @@ const router = createRouter({
     //define wraps, each route mapper should be mapped to a component
     routes: [
         { path: '/', name: 'home', component: Home },
-        { path: '/blogPosts', name: 'blogPosts', component: BlogPosts },
+        {
+          path: '/blogposts',
+          name: 'blogPosts',
+          component: BlogPosts,
+          redirect: {name: 'blogPostsGreeting'},
+          children:[  
+            { path: '/blogPostsGreeting', name: 'blogPostsGreeting', component: BlogPost },
+            { path: '/blogPosts/:id(\\d+)', name: 'blogPost', component: BlogPostsGreeting },
+           
+          ],
+        },
         { path: '/about', name: 'about', component: About },
+        {path: '/:pathMatch(.*)*',
+          name: 'notFound',
+          component: NotFound
+        },
       ],
 })
 
